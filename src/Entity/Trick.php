ddaaa36e30f1aae2e7c3d8ab4f650d9a9f43e485
Kosmik56 <lewis.kidd56@gmail.com>
2023-08-04 12:47:12 +0200

    added security routes

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\TrickRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TrickRepository::class)]
